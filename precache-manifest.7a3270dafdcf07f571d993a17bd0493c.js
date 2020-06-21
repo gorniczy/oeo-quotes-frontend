460af0be87fa5e0e14f4e97c0a2cbb2bbfc5d328
gorniczy <denysgornichar@gmail.com>
2020-06-21 19:42:27 +0200

    Updates

---
+++ precache-manifest.7a3270dafdcf07f571d993a17bd0493c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914ced50ccb60dc96217e094e8f9493b",
+    "url": "/oeo-quotes-frontend/index.html"
+  },
+  {
+    "revision": "dbc8217d5782079e771d",
+    "url": "/oeo-quotes-frontend/static/css/main.6e44b26d.chunk.css"
+  },
+  {
+    "revision": "32593fa9b67dcea38d61",
+    "url": "/oeo-quotes-frontend/static/js/2.9f114043.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/oeo-quotes-frontend/static/js/2.9f114043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc8217d5782079e771d",
+    "url": "/oeo-quotes-frontend/static/js/main.04a63c07.chunk.js"
+  },
+  {
+    "revision": "ec3212b7a9dd6cf51a05",
+    "url": "/oeo-quotes-frontend/static/js/runtime-main.7f421f9a.js"
+  },
+  {
+    "revision": "704e0bc90c12e18e474b24fef7464424",
+    "url": "/oeo-quotes-frontend/static/media/quote.704e0bc9.svg"
+  },
+  {
+    "revision": "f593aedb1c6430464512be3c683349ef",
+    "url": "/oeo-quotes-frontend/static/media/refresh.f593aedb.svg"
+  }
+]);
